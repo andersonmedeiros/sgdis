ce07c658eaad5614ae2cf49a5716f1957e31d4c8
Anderson de Paula Andrade Medeiros <andersonandrade.30@gmail.com>
2019-11-04 11:56:17 -0400

    arquivos java

---
+++ sgdis/src/java/bean/Militar.java
@@ -0,0 +1,267 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+/**
+ *
+ * @author andersondepaula
+ */
+public class Militar {
+    String identidade, nome, nome_guerra, cpf, preccp, sexo, data_nasc, data_praca, pai, mae,
+           email, nome_referencia, fone_referencia, fone1, fone2, nat_cid, end_num, senha;
+    int id, id_end, id_div_sec, id_pg, id_qq, id_ec, id_esc, id_sit, id_teleitor, id_grp_acesso,
+        id_religiao, id_nat_est, id_grp_acesso_sgdis;
+
+    public String getIdentidade() {
+        return identidade;
+    }
+
+    public void setIdentidade(String identidade) {
+        this.identidade = identidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome_guerra() {
+        return nome_guerra;
+    }
+
+    public void setNome_guerra(String nome_guerra) {
+        this.nome_guerra = nome_guerra;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getPreccp() {
+        return preccp;
+    }
+
+    public void setPreccp(String preccp) {
+        this.preccp = preccp;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getData_nasc() {
+        return data_nasc;
+    }
+
+    public void setData_nasc(String data_nasc) {
+        this.data_nasc = data_nasc;
+    }
+
+    public String getData_praca() {
+        return data_praca;
+    }
+
+    public void setData_praca(String data_praca) {
+        this.data_praca = data_praca;
+    }
+
+    public String getPai() {
+        return pai;
+    }
+
+    public void setPai(String pai) {
+        this.pai = pai;
+    }
+
+    public String getMae() {
+        return mae;
+    }
+
+    public void setMae(String mae) {
+        this.mae = mae;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome_referencia() {
+        return nome_referencia;
+    }
+
+    public void setNome_referencia(String nome_referencia) {
+        this.nome_referencia = nome_referencia;
+    }
+
+    public String getFone_referencia() {
+        return fone_referencia;
+    }
+
+    public void setFone_referencia(String fone_referencia) {
+        this.fone_referencia = fone_referencia;
+    }
+
+    public String getFone1() {
+        return fone1;
+    }
+
+    public void setFone1(String fone1) {
+        this.fone1 = fone1;
+    }
+
+    public String getFone2() {
+        return fone2;
+    }
+
+    public void setFone2(String fone2) {
+        this.fone2 = fone2;
+    }
+
+    public String getNat_cid() {
+        return nat_cid;
+    }
+
+    public void setNat_cid(String nat_cid) {
+        this.nat_cid = nat_cid;
+    }
+
+    public String getEnd_num() {
+        return end_num;
+    }
+
+    public void setEnd_num(String end_num) {
+        this.end_num = end_num;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_end() {
+        return id_end;
+    }
+
+    public void setId_end(int id_end) {
+        this.id_end = id_end;
+    }
+
+    public int getId_div_sec() {
+        return id_div_sec;
+    }
+
+    public void setId_div_sec(int id_div_sec) {
+        this.id_div_sec = id_div_sec;
+    }
+
+    public int getId_pg() {
+        return id_pg;
+    }
+
+    public void setId_pg(int id_pg) {
+        this.id_pg = id_pg;
+    }
+
+    public int getId_qq() {
+        return id_qq;
+    }
+
+    public void setId_qq(int id_qq) {
+        this.id_qq = id_qq;
+    }
+
+    public int getId_ec() {
+        return id_ec;
+    }
+
+    public void setId_ec(int id_ec) {
+        this.id_ec = id_ec;
+    }
+
+    public int getId_esc() {
+        return id_esc;
+    }
+
+    public void setId_esc(int id_esc) {
+        this.id_esc = id_esc;
+    }
+
+    public int getId_sit() {
+        return id_sit;
+    }
+
+    public void setId_sit(int id_sit) {
+        this.id_sit = id_sit;
+    }
+
+    public int getId_teleitor() {
+        return id_teleitor;
+    }
+
+    public void setId_teleitor(int id_teleitor) {
+        this.id_teleitor = id_teleitor;
+    }
+
+    public int getId_grp_acesso() {
+        return id_grp_acesso;
+    }
+
+    public void setId_grp_acesso(int id_grp_acesso) {
+        this.id_grp_acesso = id_grp_acesso;
+    }
+
+    public int getId_religiao() {
+        return id_religiao;
+    }
+
+    public void setId_religiao(int id_religiao) {
+        this.id_religiao = id_religiao;
+    }
+
+    public int getId_nat_est() {
+        return id_nat_est;
+    }
+
+    public void setId_nat_est(int id_nat_est) {
+        this.id_nat_est = id_nat_est;
+    }
+
+    public int getId_grp_acesso_sgdis() {
+        return id_grp_acesso_sgdis;
+    }
+
+    public void setId_grp_acesso_sgdis(int id_grp_acesso_sgdis) {
+        this.id_grp_acesso_sgdis = id_grp_acesso_sgdis;
+    }
+    
+    
+}
