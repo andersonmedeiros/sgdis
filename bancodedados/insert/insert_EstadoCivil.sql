1006d921c622e3e327f11926d83a5b875bdce396
Anderson de Paula Andrade Medeiros <andersonandrade.30@gmail.com>
2019-12-10 14:21:38 -0400

    insert EstadoCivil

---
+++ bancodedados/insert/insert_EstadoCivil.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM cigs_local.EstadoCivil
+LIMIT 0, 1000
+
+-- Date: 2019-12-10 14:18
+*/
+INSERT INTO `EstadoCivil` (`id`,`nome`) VALUES (1,'SOLTEIRO(A)');
+INSERT INTO `EstadoCivil` (`id`,`nome`) VALUES (2,'CASADO(A)');
+INSERT INTO `EstadoCivil` (`id`,`nome`) VALUES (3,'SEPARADO(A)');
+INSERT INTO `EstadoCivil` (`id`,`nome`) VALUES (4,'DIVORCIADO(A)');
+INSERT INTO `EstadoCivil` (`id`,`nome`) VALUES (5,'VIÚVO(A)');
+INSERT INTO `EstadoCivil` (`id`,`nome`) VALUES (6,'UNIÃO ESTÁVEL');
