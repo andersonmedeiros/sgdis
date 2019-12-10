aa8f2e75c49538c6ad04989391230a048947936d
Anderson de Paula Andrade Medeiros <andersonandrade.30@gmail.com>
2019-12-10 11:43:56 -0400

    insert table PostoGraduacao, forca EB

---
+++ bancodedados/insert/insert_PostoGraduacao.sql
@@ -0,0 +1,24 @@
+/*
+-- Query: SELECT * FROM cigs_local.PostoGraduacao
+LIMIT 0, 1000
+
+-- Date: 2019-12-10 11:43
+*/
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (1,'CIVIL','CIV',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (2,'SOLDADO EV','SD EV',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (3,'SOLDADO EP','SD EP',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (4,'CABO','CB',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (5,'3º SARGENTO','3º SGT',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (6,'2º SARGENTO','2º SGT',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (7,'1º SARGENTO','1º SGT',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (8,'SUBTENENTE','S TEN',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (9,'ASPIRANTE A OFICIAL','ASP',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (10,'2º TENENTE','2º TEN',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (11,'1º TENENTE','1º TEN',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (12,'CAPITÃO','CAP',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (13,'MAJOR','MAJ',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (14,'TENENTE-CORONEL','TEN CEL',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (15,'CORONEL','CEL',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (16,'GENERAL DE BRIGADA','GEN BDA',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (17,'GENERAL DE DIVISÃO','GEN DIV',2);
+INSERT INTO `PostoGraduacao` (`id`,`nome`,`abreviatura`,`idForcaArmada`) VALUES (18,'GENERAL DE EXÉRCITO','GEN EX',2);
