359c09ff68d2620a26cb055a5584aa2e7b60f73b
Anderson de Paula Andrade Medeiros <andersonandrade.30@gmail.com>
2019-12-10 13:54:49 -0400

    insert table RegiaoMilitar, forca EB

---
+++ bancodedados/insert/insert_RegiaoMilitar.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM cigs_local.RegiaoMilitar
+LIMIT 0, 1000
+
+-- Date: 2019-12-10 13:54
+*/
+INSERT INTO `RegiaoMilitar` (`id`,`nome`,`sigla`,`idForcaArmada`) VALUES (1,'1ª REGIÃO MILITAR','1ª RM',2);
+INSERT INTO `RegiaoMilitar` (`id`,`nome`,`sigla`,`idForcaArmada`) VALUES (2,'2ª REGIÃO MILITAR','2ª RM',2);
+INSERT INTO `RegiaoMilitar` (`id`,`nome`,`sigla`,`idForcaArmada`) VALUES (3,'3ª REGIÃO MILITAR','3ª RM',2);
+INSERT INTO `RegiaoMilitar` (`id`,`nome`,`sigla`,`idForcaArmada`) VALUES (4,'4ª REGIÃO MILITAR','4ª RM',2);
+INSERT INTO `RegiaoMilitar` (`id`,`nome`,`sigla`,`idForcaArmada`) VALUES (5,'5ª REGIÃO MILITAR','5ª RM',2);
+INSERT INTO `RegiaoMilitar` (`id`,`nome`,`sigla`,`idForcaArmada`) VALUES (6,'6ª REGIÃO MILITAR','6ª RM',2);
+INSERT INTO `RegiaoMilitar` (`id`,`nome`,`sigla`,`idForcaArmada`) VALUES (7,'7ª REGIÃO MILITAR','7ª RM',2);
+INSERT INTO `RegiaoMilitar` (`id`,`nome`,`sigla`,`idForcaArmada`) VALUES (8,'8ª REGIÃO MILITAR','8ª RM',2);
+INSERT INTO `RegiaoMilitar` (`id`,`nome`,`sigla`,`idForcaArmada`) VALUES (9,'9ª REGIÃO MILITAR','9ª RM',2);
+INSERT INTO `RegiaoMilitar` (`id`,`nome`,`sigla`,`idForcaArmada`) VALUES (10,'10ª REGIÃO MILITAR','10ª RM',2);
+INSERT INTO `RegiaoMilitar` (`id`,`nome`,`sigla`,`idForcaArmada`) VALUES (11,'11ª REGIÃO MILITAR','11ª RM',2);
+INSERT INTO `RegiaoMilitar` (`id`,`nome`,`sigla`,`idForcaArmada`) VALUES (12,'12ª REGIÃO MILITAR','12ª RM',2);
