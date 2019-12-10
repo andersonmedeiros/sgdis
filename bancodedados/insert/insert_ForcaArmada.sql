d775a0a6b1beb1d957f5169282216b14e1f46dcf
Anderson de Paula Andrade Medeiros <andersonandrade.30@gmail.com>
2019-12-10 11:26:45 -0400

    insert table ForcaArmada

---
+++ bancodedados/insert/insert_ForcaArmada.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM cigs_local.ForcaArmada
+LIMIT 0, 1000
+
+-- Date: 2019-12-10 11:25
+*/
+INSERT INTO `ForcaArmada` (`id`,`nome`,`sigla`) VALUES (1,'MARINHA DO BRASIL','MB');
+INSERT INTO `ForcaArmada` (`id`,`nome`,`sigla`) VALUES (2,'EXÉRCITO BRASILEIRO','EB');
+INSERT INTO `ForcaArmada` (`id`,`nome`,`sigla`) VALUES (3,'FORÇA AÉREA BRASILEIRA','FAB');
