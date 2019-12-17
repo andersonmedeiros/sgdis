f7867767b85aa140f63323b119b707d2b575de21
Anderson de Paula Andrade Medeiros <andersonandrade.30@gmail.com>
2019-12-17 11:19:01 -0400

    update banco de dados

---
+++ bancodedados/insert/insert_TESTE.sql
@@ -0,0 +1,33 @@
+/*
+-- Query: SELECT * FROM cigs.QAS_QMS
+LIMIT 0, 1000
+
+-- Date: 2019-12-10 13:14
+*/
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (1,'INFANTARIA');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (2,'CAVALARIA');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (3,'ARTILHARIA');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (4,'ENGENHARIA');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (5,'COMUNICAÇÕES');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (6,'MATERIAL BÉLICO');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (7,'INTENDÊNCIA');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (8,'ENGENHEIRO MILITAR');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (9,'SAÚDE');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (10,'QUADRO AUXILIAR DE OFICIAIS');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (11,'ASSISTÊNCIA RELIGIOSA');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (12,'QUADRO COMPLEMENTAR DE OFICIAIS');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (13,'QUADRO TÉCNICO TEMPORÁRIO');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (14,'NÃO QUALIFICADO');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (15,'TOPÓGRAFO');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (16,'CLARIM');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (17,'COMANDOS');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (18,'MÚSICO');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (19,'TAIFEIRO');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (21,'QUADRO ESPECIAL');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (22,'MÉDICO');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (23,'DENTISTA');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (24,'CAPELÃO CATÓLICO');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (25,'AVIAÇÃO');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (27,'CORNETEIRO');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (28,'CAPELÃO PROTESTANTE');
+INSERT INTO `QAS_QMS` (`Arma_id`,`Arma_desc`) VALUES (29,'FARMACÊUTICO');
