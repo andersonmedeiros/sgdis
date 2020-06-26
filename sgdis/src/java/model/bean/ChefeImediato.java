fcdb49ec29a169e40ca27c0d50ea876699405c37
Anderson de Paula Andrade Medeiros <andersonandrade.30@gmail.com>
2020-06-26 03:52:45 -0400

    model: classes bean e dao de Chefe Imediato

---
+++ sgdis/src/java/model/bean/ChefeImediato.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bean;
+
+/**
+ *
+ * @author anderson
+ */
+public class ChefeImediato {
+    private int id;
+    private String nome, sobrenome, nomeguerra;
+    private final PostoGraduacao pg = new PostoGraduacao();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNomeguerra() {
+        return nomeguerra;
+    }
+
+    public void setNomeguerra(String nomeguerra) {
+        this.nomeguerra = nomeguerra;
+    }
+    
+    public int getIdPostoGraduacao() {
+        return pg.getId();
+    }
+
+    public void setIdPostoGraduacao(int idPostoGraduacao) {
+        pg.setId(idPostoGraduacao);
+    }
+
+    public String getNomePostoGraduacao() {
+        return pg.getNome();
+    }
+
+    public void setNomePostoGraduacao(String nomePostoGraduacao) {
+        pg.setNome(nomePostoGraduacao);
+    }
+
+    public String getAbreviaturaPostoGraduacao() {
+        return pg.getAbreviatura();
+    }
+
+    public void setAbreviaturaPostoGraduacao(String abreviaturaPostoGraduacao) {
+        pg.setAbreviatura(abreviaturaPostoGraduacao);
+    }
+
+    public String getCirculohierarquicoPostoGraduacao() {
+        return pg.getCirculohierarquico();
+    }
+
+    public void setCirculohierarquicoPostoGraduacao(String circulohierarquicoPostoGraduacao) {
+        pg.setCirculohierarquico(circulohierarquicoPostoGraduacao);
+    }
+    
+    public int getIdForcaPostoGraduacao() {
+        return pg.getIdForca();
+    }
+
+    public void setIdForcaPostoGraduacao(int idForca) {
+        pg.setIdForca(idForca);
+    }
+
+    public String getNomeForcaPostoGraduacao() {
+        return pg.getNomeForca();
+    }
+
+    public void setNomeForcaPostoGraduacao(String nomeForca) {
+        pg.setNomeForca(nomeForca);
+    }
+
+    public String getSiglaForcaPostoGraduacao() {
+        return pg.getSiglaForca();
+    }
+
+    public void setSiglaForcaPostoGraduacao(String siglaForca) {
+        pg.setSiglaForca(siglaForca);
+    }
+    
+    public int getIdCategoriaPostoGraduacao() {
+        return pg.getIdCategoria();
+    }
+
+    public void setIdCategoriaPostoGraduacao(int idCategoria) {
+        pg.setIdCategoria(idCategoria);
+    }
+
+    public String getNomeCategoriaPostoGraduacao() {
+        return pg.getNomeCategoria();
+    }
+
+    public void setNomeCategoriaPostoGraduacao(String nomeCategoria) {
+        pg.setNomeCategoria(nomeCategoria);
+    }
+
+    public String getDescricaoCategoriaPostoGraduacao() {
+        return pg.getDescricaoCategoria();
+    }
+
+    public void setDescricaoCategoriaPostoGraduacao(String descricaoCategoria) {
+        pg.setDescricaoCategoria(descricaoCategoria);
+    }
+}
---
+++ sgdis/src/java/model/dao/ChefeImediatoDAO.java
@@ -0,0 +1,226 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.dao;
+
+import connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.bean.ChefeImediato;
+import model.bean.PostoGraduacao;
+
+/**
+ *
+ * @author anderson
+ */
+public class ChefeImediatoDAO {
+    //Tabela
+    String tabela = "ChImediato";
+    
+    //Atributos
+    String id = "id";
+    String nome = "nome";
+    String sobrenome = "sobrenome";
+    String nomeguerra = "nomeguerra";
+    String idPostoGraduacao = "idPostoGraduacao";
+    
+    //Insert SQL
+    private final String INSERT = "INSERT INTO " + tabela + "(" + id + "," + nome + "," + sobrenome + "," + nomeguerra + "," + idPostoGraduacao + ")" +
+                                  " VALUES(?,?,?,?,?);";
+    
+    //Update SQL
+    private final String UPDATE = "UPDATE " + tabela +
+                                  " SET " + nome + "=?, " + sobrenome + "=?, " + nomeguerra + "=?, " + idPostoGraduacao + "=?, " +
+                                  "WHERE " + id + "=?;";
+        
+    //Delete SQL
+    private final String DELETE = "DELETE FROM " + tabela + " WHERE " + id + "=?;";
+    
+    //Consultas SQL
+    private final String GETUltimoID = "SELECT MAX(" + id + ") as ultimo_id FROM " + tabela + ";";
+    
+    
+    Connection conn = null;
+    PreparedStatement pstm = null;
+    ResultSet rs = null;
+    
+    //Próximo ID a ser inserido
+    public int proxID(){
+        int ultimo_id = 0;
+        try{
+            conn = ConnectionFactory.getConnection();
+            
+            pstm = conn.prepareStatement(GETUltimoID);
+            rs = pstm.executeQuery();
+            while (rs.next()) {
+                
+                ultimo_id = rs.getInt("ultimo_id");
+            }
+           
+            ConnectionFactory.fechaConexao(conn, pstm);
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());           
+        }
+        return (ultimo_id+1);
+    }
+    
+    //Insert SQL
+    public void insert(ChefeImediato chImto) {
+        if (chImto != null) {
+            try {
+                conn = ConnectionFactory.getConnection();
+                
+                pstm = conn.prepareStatement(INSERT);
+                
+                pstm.setInt(1, chImto.getId());
+                pstm.setString(2, chImto.getNome());
+                pstm.setString(3, chImto.getSobrenome());
+                pstm.setString(4, chImto.getNomeguerra());
+                pstm.setInt(5, chImto.getIdPostoGraduacao());
+                                                              
+                pstm.execute();
+                
+                ConnectionFactory.fechaConexao(conn, pstm);
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e.getMessage());  
+            }
+        } else {
+            throw new RuntimeException();
+        }
+    }
+    
+    //Update SQL
+    public void update(ChefeImediato chImto) {
+        if (chImto != null) {
+            try {
+                conn = ConnectionFactory.getConnection();
+                pstm = conn.prepareStatement(UPDATE);                
+                
+                pstm.setString(1, chImto.getNome());
+                pstm.setString(2, chImto.getSobrenome());
+                pstm.setString(3, chImto.getNomeguerra());
+                pstm.setInt(4, chImto.getIdPostoGraduacao());
+                pstm.setInt(5, chImto.getId());
+            
+                pstm.execute();
+                ConnectionFactory.fechaConexao(conn, pstm);
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e.getMessage());  
+            }
+        } else {            
+            throw new RuntimeException();
+        }
+    }
+    
+    //Delete SQL
+    public void delete(int id) {
+        if (id != 0){
+            try {
+                conn = ConnectionFactory.getConnection();
+                pstm = conn.prepareStatement(DELETE);
+                pstm.setInt(1, id);
+            
+                pstm.execute();
+                ConnectionFactory.fechaConexao(conn, pstm);
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e.getMessage());  
+            }
+        } else {            
+            throw new RuntimeException();
+        }
+    }
+    
+    private final String GETCHIMTO = "SELECT * " +
+                                    "FROM ChImediato " + 
+                                    "WHERE id = ?";
+       
+    public ChefeImediato getChefeImediatoById(int idCmt){
+        ChefeImediato chImto = new ChefeImediato();   
+        PostoGraduacaoDAO pgDAO = new PostoGraduacaoDAO();
+        try {
+            conn = ConnectionFactory.getConnection();
+            pstm = conn.prepareStatement(GETCHIMTO);
+            pstm.setInt(1, idCmt);
+           
+            rs = pstm.executeQuery();
+            while (rs.next()) {
+                PostoGraduacao pg = pgDAO.getPostoGraduacao(rs.getInt("idPostoGraduacao"));
+                
+                chImto.setId(rs.getInt("id"));
+                chImto.setNome(rs.getString("nome"));
+                chImto.setSobrenome(rs.getString("sobrenome"));
+                chImto.setNomeguerra(rs.getString("nomeguerra"));
+                
+                chImto.setIdPostoGraduacao(pg.getId());
+                chImto.setNomePostoGraduacao(pg.getNome());
+                chImto.setAbreviaturaPostoGraduacao(pg.getAbreviatura());
+                chImto.setCirculohierarquicoPostoGraduacao(pg.getCirculohierarquico());
+                
+                chImto.setIdForcaPostoGraduacao(pg.getIdForca());
+                chImto.setNomeForcaPostoGraduacao(pg.getNomeForca());
+                chImto.setSiglaForcaPostoGraduacao(pg.getSiglaForca());
+                
+                chImto.setIdCategoriaPostoGraduacao(pg.getIdCategoria());
+                chImto.setNomeCategoriaPostoGraduacao(pg.getNomeCategoria());
+                chImto.setDescricaoCategoriaPostoGraduacao(pg.getDescricaoCategoria());
+            }
+            ConnectionFactory.fechaConexao(conn, pstm, rs);
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());           
+        }
+        return chImto;
+    }
+    
+    private final String GETCHIMTOEXISTENTE = "SELECT * " + 
+                                           " FROM " + tabela + 
+                                           " WHERE " + nome + "=? AND " + sobrenome + "=? AND " + nomeguerra + "=? AND " + idPostoGraduacao +"=? "+";";
+
+    public ChefeImediato getChefeImediatoExistente(String nome, String sobrenome, String nomeguerra, int idPGrad){
+        ChefeImediato chImto = new ChefeImediato();   
+        PostoGraduacaoDAO pgDAO = new PostoGraduacaoDAO();
+        try {
+            conn = ConnectionFactory.getConnection();
+            pstm = conn.prepareStatement(GETCHIMTOEXISTENTE);
+            pstm.setString(1, nome);
+            pstm.setString(2, sobrenome);
+            pstm.setString(3, nomeguerra);
+            pstm.setInt(4, idPGrad);
+           
+            rs = pstm.executeQuery();
+            while (rs.next()) {
+                PostoGraduacao pg = pgDAO.getPostoGraduacao(rs.getInt("idPostoGraduacao"));
+                
+                chImto.setId(rs.getInt("id"));
+                chImto.setNome(rs.getString("nome"));
+                chImto.setSobrenome(rs.getString("sobrenome"));
+                chImto.setNomeguerra(rs.getString("nomeguerra"));
+                
+                chImto.setIdPostoGraduacao(pg.getId());
+                chImto.setNomePostoGraduacao(pg.getNome());
+                chImto.setAbreviaturaPostoGraduacao(pg.getAbreviatura());
+                chImto.setCirculohierarquicoPostoGraduacao(pg.getCirculohierarquico());
+                
+                chImto.setIdForcaPostoGraduacao(pg.getIdForca());
+                chImto.setNomeForcaPostoGraduacao(pg.getNomeForca());
+                chImto.setSiglaForcaPostoGraduacao(pg.getSiglaForca());
+                
+                chImto.setIdCategoriaPostoGraduacao(pg.getIdCategoria());
+                chImto.setNomeCategoriaPostoGraduacao(pg.getNomeCategoria());
+                chImto.setDescricaoCategoriaPostoGraduacao(pg.getDescricaoCategoria());
+                
+                return chImto;
+            }
+            ConnectionFactory.fechaConexao(conn, pstm, rs);
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());           
+        }
+        return null;
+    }
+}
