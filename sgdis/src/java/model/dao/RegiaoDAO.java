5597a317beacbeae7d140617ba00d2b6a756d9cc
Anderson de Paula Andrade Medeiros <andersonandrade.30@gmail.com>
2020-06-27 15:22:46 -0400

    model: classes bean e dao de Região

---
+++ sgdis/src/java/model/dao/RegiaoDAO.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.dao;
+
+import connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import model.bean.Regiao;
+
+/**
+ *
+ * @author anderson
+ */
+public class RegiaoDAO {
+    //Tabela
+    String tabela = "Regiao";
+    
+    //Atributos
+    String id = "id";
+    String nome = "nome";
+    
+    //Insert SQL
+    private final String INSERT = "INSERT INTO " + tabela + "(" + id + "," + nome + ")" +
+                                  " VALUES(?,?);";
+    
+    //Update SQL
+    private final String UPDATE = "UPDATE " + tabela +
+                                  " SET " + nome + "=?, " +
+                                  "WHERE " + id + "=?;";
+        
+    //Delete SQL
+    private final String DELETE = "DELETE FROM " + tabela + " WHERE " + id + "=?;";
+    
+    //Consultas SQL
+    
+    Connection conn = null;
+    PreparedStatement pstm = null;
+    ResultSet rs = null;
+    
+    
+    //Insert SQL
+    public void insert(Regiao r) {
+        if (r != null) {
+            try {
+                conn = ConnectionFactory.getConnection();
+                
+                pstm = conn.prepareStatement(INSERT);
+                
+                pstm.setInt(1, r.getId());
+                pstm.setString(2, r.getNome());
+                                                              
+                pstm.execute();
+                
+                ConnectionFactory.fechaConexao(conn, pstm);
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e.getMessage());  
+            }
+        } else {
+            throw new RuntimeException();
+        }
+    }
+    
+    //Update SQL
+    public void update(Regiao r) {
+        if (r != null) {
+            try {
+                conn = ConnectionFactory.getConnection();
+                pstm = conn.prepareStatement(UPDATE);
+                
+                pstm.setString(1, r.getNome());
+                pstm.setInt(2, r.getId());
+            
+                pstm.execute();
+                ConnectionFactory.fechaConexao(conn, pstm);
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e.getMessage());  
+            }
+        } else {            
+            throw new RuntimeException();
+        }
+    }
+    
+    //Delete SQL
+    public void delete(int id) {
+        if (id != 0){
+            try {
+                conn = ConnectionFactory.getConnection();
+                pstm = conn.prepareStatement(DELETE);
+                pstm.setInt(1, id);
+            
+                pstm.execute();
+                ConnectionFactory.fechaConexao(conn, pstm);
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e.getMessage());  
+            }
+        } else {            
+            throw new RuntimeException();
+        }
+    }
+    
+    private final String GETREGIAOBYID = "SELECT * " +
+                                      "FROM " + tabela + " " +
+                                      "WHERE id = ?;";
+       
+    public Regiao getRegiaoById(int idREGIAO){
+        Regiao r = new Regiao();        
+        try {
+            conn = ConnectionFactory.getConnection();
+            pstm = conn.prepareStatement(GETREGIAOBYID);
+            pstm.setInt(1, idREGIAO);
+           
+            rs = pstm.executeQuery();
+            while (rs.next()) {
+                r.setId(rs.getInt("id"));
+                r.setNome(rs.getString("nome"));
+            }
+            ConnectionFactory.fechaConexao(conn, pstm, rs);
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());           
+        }
+        return r;
+    }
+    
+    private final String GETREGIOES = "SELECT * " +
+                                   "FROM " + tabela;
+       
+    public ArrayList<Regiao> getRegiaos(){
+        ArrayList<Regiao> regioes = new ArrayList<>();        
+        try {
+            conn = ConnectionFactory.getConnection();
+            pstm = conn.prepareStatement(GETREGIOES);
+           
+            rs = pstm.executeQuery();
+            while (rs.next()) {
+                Regiao r = new Regiao();
+                
+                r.setId(rs.getInt("id"));
+                r.setNome(rs.getString("nome"));
+                
+                regioes.add(r);
+            }
+            ConnectionFactory.fechaConexao(conn, pstm, rs);
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());           
+        }
+        return regioes;
+    }
+}
