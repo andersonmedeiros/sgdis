ffb4b93386b86a73c01c17b9d9b86584be74dacd
Anderson de Paula Andrade Medeiros <andersonandrade.30@gmail.com>
2020-06-27 23:23:32 -0400

    model: classes bean e dao Titulo Eleitor, add método by idtAluno

--- sgdis/src/java/model/dao/TituloEleitorDAO.java
+++ sgdis/src/java/model/dao/TituloEleitorDAO.java
@@ -117,18 +117,88 @@ public class TituloEleitorDAO {
         }
     }
     
-    private final String GETTITULOELEITORBYID = "SELECT * " +
-                                                "FROM Endereco " + 
-                                                "WHERE id = ?";
+    private final String GETTITULOELEITORBYREGISTRO = "SELECT * " +
+                                                "FROM TituloEleitor " + 
+                                                "WHERE registro = ?";
        
-    public TituloEleitor getTituloEleitorById(int idTitEleitor){
+    public TituloEleitor getTituloEleitorByRegistro(String registro){
         TituloEleitor titEleitor = new TituloEleitor();
         CidadeDAO cidDAO = new CidadeDAO();
         AlunoDAO alDAO = new AlunoDAO();
         try {
             conn = ConnectionFactory.getConnection();
-            pstm = conn.prepareStatement(GETTITULOELEITORBYID);
-            pstm.setInt(1, idTitEleitor);
+            pstm = conn.prepareStatement(GETTITULOELEITORBYREGISTRO);
+            pstm.setString(1, registro);
+           
+            rs = pstm.executeQuery();
+            while (rs.next()) {
+                titEleitor.setRegistro(rs.getString("registro"));
+                titEleitor.setZona(rs.getString("zona"));
+                titEleitor.setSecao(rs.getString("secao"));
+                
+                Cidade cid = cidDAO.getCidadeById(rs.getInt("idCidade"));
+                titEleitor.setIdCidade(cid.getId());
+                titEleitor.setNomeCidade(cid.getNome());
+                titEleitor.setIdEstadoCidade(cid.getIdEstado());
+                titEleitor.setNomeEstadoCidade(cid.getNomeEstado());
+                titEleitor.setSiglaEstadoCidade(cid.getSiglaEstado());
+                titEleitor.setIdRegiaoEstadoCidade(cid.getIdRegiaoEstado());
+                titEleitor.setNomeRegiaoEstadoCidade(cid.getNomeRegiaoEstado());
+                
+                Aluno al = alDAO.getAlunoByIdentidade(rs.getString("idtAluno"));
+                titEleitor.setIdentidadeAluno(al.getIdentidade());
+                titEleitor.setSituacaoAluno(al.getSituacao());
+                titEleitor.setIdPostoGraduacaoAluno(al.getIdPostoGraduacao());
+                titEleitor.setIdQasQmsAluno(al.getIdQasQms());
+                titEleitor.setIdCmtAluno(al.getIdCmt());
+                titEleitor.setDataNascimentoAluno(al.getDataNascimento());
+                titEleitor.setNomeAluno(al.getNome());
+                titEleitor.setSobrenomeAluno(al.getSobrenome());
+                titEleitor.setNomeguerraAluno(al.getNomeguerra());
+                titEleitor.setPreccpAluno(al.getPreccp());
+                titEleitor.setCpAluno(al.getCp());
+                titEleitor.setCpfAluno(al.getCpf());
+                titEleitor.setUltDataPracaAluno(al.getUltDataPraca());
+                titEleitor.setIdNatCidadeAluno(al.getIdNatCidade());
+                titEleitor.setIdEstadoCivilAluno(al.getIdEstadoCivil());
+                titEleitor.setTsAluno(al.getTs());
+                titEleitor.setFtrhAluno(al.getFtrh());
+                titEleitor.setPaiAluno(al.getPai());
+                titEleitor.setMaeAluno(al.getMae());
+                titEleitor.setEmailAluno(al.getEmail());
+                titEleitor.setFumanteAluno(al.getFumante());
+                titEleitor.setIdOMAluno(al.getIdOM());
+                titEleitor.setIdComportamentoAluno(al.getIdComportamento());
+                titEleitor.setIdChImtoAluno(al.getIdChImto());
+                titEleitor.setIdFormOrigemAluno(al.getIdFormOrigem());
+                titEleitor.setSexoAluno(al.getSexo());
+                titEleitor.setUltfuncao1Aluno(al.getUltfuncao1());
+                titEleitor.setUltfuncao2Aluno(al.getUltfuncao2());
+                titEleitor.setUltfuncao3Aluno(al.getUltfuncao3());
+                titEleitor.setIdTafAluno(al.getIdTaf());
+                titEleitor.setIdPromocaoAluno(al.getIdPromocao());
+                titEleitor.setIdPreparacaoAluno(al.getIdPreparacao());
+                titEleitor.setIdUniformeAluno(al.getIdUniforme());
+            }
+            ConnectionFactory.fechaConexao(conn, pstm, rs);
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());           
+        }
+        return titEleitor;
+    }
+    
+    private final String GETTITULOELEITORBYIDTALUNO = "SELECT * " +
+                                                      "FROM TituloEleitor " + 
+                                                      "WHERE idtAluno = ?";
+       
+    public TituloEleitor getTituloEleitorByIdtAluno(int idtAluno){
+        TituloEleitor titEleitor = new TituloEleitor();
+        CidadeDAO cidDAO = new CidadeDAO();
+        AlunoDAO alDAO = new AlunoDAO();
+        try {
+            conn = ConnectionFactory.getConnection();
+            pstm = conn.prepareStatement(GETTITULOELEITORBYIDTALUNO);
+            pstm.setInt(1, idtAluno);
            
             rs = pstm.executeQuery();
             while (rs.next()) {
